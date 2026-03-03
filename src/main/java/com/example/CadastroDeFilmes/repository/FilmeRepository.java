64a2cd9c500c2989916c6aaf653f11ff7151386a
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2026-03-03 17:16:49 -0300

    Interface de repositório criada

---
+++ src/main/java/com/example/CadastroDeFilmes/repository/FilmeRepository.java
@@ -0,0 +1,9 @@
+package com.example.CadastroDeFilmes.repository;
+
+import com.example.CadastroDeFilmes.entity.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+}
