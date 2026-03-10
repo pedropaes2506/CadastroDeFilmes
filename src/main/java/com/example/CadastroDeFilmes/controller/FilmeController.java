9656d9595c6d3bb022ea4d0c5b1ac489ea6affa4
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2026-03-10 14:26:08 -0300

    Classe Controller criada com os métodos para salvar, listar todos, buscar por id e deletar

---
+++ src/main/java/com/example/CadastroDeFilmes/controller/FilmeController.java
@@ -0,0 +1,37 @@
+package com.example.CadastroDeFilmes.controller;
+
+import com.example.CadastroDeFilmes.entity.Filme;
+import com.example.CadastroDeFilmes.service.FilmeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/filmes")
+@RequiredArgsConstructor
+public class FilmeController{
+
+    private final FilmeService filmeService;
+
+    @PostMapping
+    public Filme salvar(@RequestBody Filme filme) {
+        return filmeService.salvar(filme);
+    }
+
+    @GetMapping
+    public List<Filme> listarTodos() {
+        return filmeService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Filme buscarPorId(@PathVariable Long id) {
+        return filmeService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        filmeService.deletar(id);
+    }
+
+}
